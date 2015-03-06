233db60dff05bd09f65b2f9e15823dc484b924b6
Kazuaki Hara <bizhara@gmail.com>
2015-03-06 17:41:34 +0900

    UIView+FromNib 追加

---
+++ UIView+FromNib.h
@@ -0,0 +1,13 @@
+//
+//  UIView+FromNib.h
+//  Quelon-Proto
+//
+//  Created by hara on 2015/03/06.
+//  Copyright (c) 2015年 quelon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (FromNib)
+
+@end
---
+++ UIView+FromNib.m
@@ -0,0 +1,13 @@
+//
+//  UIView+FromNib.m
+//  Quelon-Proto
+//
+//  Created by hara on 2015/03/06.
+//  Copyright (c) 2015年 quelon. All rights reserved.
+//
+
+#import "UIView+FromNib.h"
+
+@implementation UIView (FromNib)
+
+@end
