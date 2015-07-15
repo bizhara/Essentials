3df35101644719a6dbe7bb63beb2f2ddb8b9057e
Kazuaki Hara <bizhara@gmail.com>
2015-07-15 18:51:43 +0900

    シンプルな WebView + ViewController

---
+++ WebViewController.h
@@ -0,0 +1,15 @@
+//
+//  WebViewController.h
+//  QuelonApp
+//
+//  Created by hara on 2015/07/15.
+//  Copyright (c) 2015年 Quelon Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/// Web view controller
+@interface WebViewController : UIViewController
++ (instancetype)viewControllerWithUrlString:(NSString *)urlString title:(NSString *)title;
+
+@end
---
+++ WebViewController.m
@@ -0,0 +1,34 @@
+//
+//  WebViewController.m
+//  QuelonApp
+//
+//  Created by hara on 2015/07/15.
+//  Copyright (c) 2015年 Quelon Inc. All rights reserved.
+//
+
+#import "WebViewController.h"
+#import "UIViewController+FromNib.h"
+
+@interface WebViewController () <UIWebViewDelegate>
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+@property (copy, nonatomic) NSString *urlString;
+
+@end
+
+@implementation WebViewController
+
++ (instancetype)viewControllerWithUrlString:(NSString *)urlString title:(NSString *)title {
+    WebViewController *me = [self viewControllerFromNib];
+    me.urlString = urlString;
+    me.title = title;
+    return me;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:self.urlString]];
+    [self.webView loadRequest:request];
+}
+
+@end
