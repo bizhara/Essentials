9d56e1664a2b15690a9ea52746c1577d7f19d10d
Kazuaki Hara <bizhara@gmail.com>
2015-07-15 19:53:52 +0900

    戻るボタンのタイトルを非表示にする

---
+++ UINavigationItem+WithoutBackbuttonTitle.h
@@ -0,0 +1,15 @@
+//
+//  UINavigationItem+WithoutBackbuttonTitle.h
+//  QuelonApp
+//
+//  Created by hara on 2015/07/15.
+//  Copyright (c) 2015年 Quelon Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/// 次に表示される ViewController の戻るボタンのタイトルを非表示にする（遷移元の ViewController で呼び出すこと）
+@interface UINavigationItem (WithoutBackbuttonTitle)
+- (void)withoutBackbuttonTitle;
+
+@end
---
+++ UINavigationItem+WithoutBackbuttonTitle.m
@@ -0,0 +1,18 @@
+//
+//  UINavigationItem+WithoutBackbuttonTitle.m
+//  QuelonApp
+//
+//  Created by hara on 2015/07/15.
+//  Copyright (c) 2015年 Quelon Inc. All rights reserved.
+//
+
+#import "UINavigationItem+WithoutBackbuttonTitle.h"
+
+@implementation UINavigationItem (WithoutBackbuttonTitle)
+
+- (void)withoutBackbuttonTitle {
+    // 次に表示される ViewController の戻るボタンの文字列を消すには、遷移元の ViewController のタイトルを消しておく必要がある
+    self.backBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"" style:UIBarButtonItemStylePlain target:nil action:nil];
+}
+
+@end
