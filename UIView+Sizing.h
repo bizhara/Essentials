9e76987a42cef2f98026399da68e79d9911bdf73
Kazuaki Hara <bizhara@gmail.com>
2015-07-24 21:20:30 +0900

    Label のみならずより汎用的に使えるように

--- UIView+Sizing.h
+++ UIView+Sizing.h
@@ -1,5 +1,5 @@
 //
-//  UILabel+Sizing.h
+//  UIView+Sizing.h
 //
 //  Created by hara on 2015/06/20.
 //  Copyright (c) 2015年 Quelon Inc. All rights reserved.
@@ -7,8 +7,8 @@
 
 #import <UIKit/UIKit.h>
 
-/// UILabel サイズ調整関連
-@interface UILabel (Sizing)
+/// UIView サイズ調整関連
+@interface UIView (Sizing)
 /// 高さを文字数または行数に合わせて調整
 - (void)sizeToFitV;
 
--- UIView+Sizing.m
+++ UIView+Sizing.m
@@ -1,16 +1,16 @@
 //
-//  UILabel+Sizing.m
+//  UIView+Sizing.m
 //
 //  Created by hara on 2015/06/20.
 //  Copyright (c) 2015年 Quelon Inc. All rights reserved.
 //
 
-#import "UILabel+Sizing.h"
+#import "UIView+Sizing.h"
 
-@implementation UILabel (Sizing)
+@implementation UIView (Sizing)
 
 - (void)sizeToFitV {
-    CGSize newSize = [self sizeThatFits:CGSizeMake(self.frame.size.width, 9999)];
+    CGSize newSize = [self sizeThatFits:CGSizeMake(self.frame.size.width, CGFLOAT_MAX)];
     self.frame = CGRectMake(self.frame.origin.x, self.frame.origin.y, self.frame.size.width, newSize.height);
 }
 
